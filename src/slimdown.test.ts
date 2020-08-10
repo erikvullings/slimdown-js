32b38c493fe9de5d723d3a98cdc2a5d0b530971c
Erik Vullings <erik.vullings@gmail.com>
2020-08-10 15:09:27 +0200

    Added test for underscores.

--- src/slimdown.test.ts
+++ src/slimdown.test.ts
@@ -3,13 +3,19 @@ import test from 'ava';
 
 const removeWhitespaces = (txt: string) => txt.replace(/\s+/g, '');
 
-test('parsing header', t => {
+test('parsing header', (t) => {
   const expected = '<h1>Hello world</h1>';
   const html = render('# Hello world');
   t.is(removeWhitespaces(html), removeWhitespaces(expected));
 });
 
-test('parsing strong in own paragraph', t => {
+test('process underscores', (t) => {
+  const expected = '<p>Hello_world</p>';
+  const html = render('Hello_world');
+  t.is(removeWhitespaces(html), removeWhitespaces(expected));
+});
+
+test('parsing strong in own paragraph', (t) => {
   const md = `An **indie electronica music** bundle.
 
   **Featuring** songs by ...
@@ -30,7 +36,7 @@ Pay what you want for Music
   t.is(generated, expected);
 });
 
-test('parsing longer text', t => {
+test('parsing longer text', (t) => {
   const md = `# Title
 
 To use **Slimdown**, grap it from [npm](https://www.npmjs.com/package/slimdown-js) or *fork* the project on [GitHub](https://github.com/erikvullings/slimdown-js).
@@ -96,7 +102,7 @@ More text...
   t.is(removeWhitespaces(html), removeWhitespaces(expected));
 });
 
-test('parsing links with underscores', t => {
+test('parsing links with underscores', (t) => {
   const md = `# Links fail with underscores
 
   [Test Link](http://www.google.com/?some_param=another_value)`;
@@ -106,28 +112,26 @@ test('parsing links with underscores', t => {
   t.is(removeWhitespaces(html), removeWhitespaces(expected));
 });
 
-test('parsing images', t => {
+test('parsing images', (t) => {
   const md = `NS logo image: ![ns logo](https://www.ns.nl/static/generic/2.49.1/images/nslogo.svg)`;
-  const expected =
-    `<p>NS logo image: <img src='https://www.ns.nl/static/generic/2.49.1/images/nslogo.svg' alt='ns logo'></p>`;
+  const expected = `<p>NS logo image: <img src='https://www.ns.nl/static/generic/2.49.1/images/nslogo.svg' alt='ns logo'></p>`;
   const html = render(md);
   t.is(removeWhitespaces(html), removeWhitespaces(expected));
 });
 
-test('parsing code blocks', t => {
+test('parsing code blocks', (t) => {
   const md = `# Code example
 \`\`\`
 Tab indented
 codeblock
 \`\`\`
 `;
-  const expected =
-    `<h1>Code example</h1><pre>Tab indented codeblock</pre>`;
+  const expected = `<h1>Code example</h1><pre>Tab indented codeblock</pre>`;
   const html = render(md);
   t.is(removeWhitespaces(html), removeWhitespaces(expected));
 });
 
-test('parsing tables', t => {
+test('parsing tables', (t) => {
   const md = `# Table example
 
 | Tables        | Are           | Cool  |
@@ -136,8 +140,7 @@ test('parsing tables', t => {
 | col 2 is      | centered      |   $12 |
 | zebra stripes | are neat      |    $1 |
 `;
-  const expected =
-    `<h1>Table example</h1>
+  const expected = `<h1>Table example</h1>
     <table>
       <tbody>
         <tr>
